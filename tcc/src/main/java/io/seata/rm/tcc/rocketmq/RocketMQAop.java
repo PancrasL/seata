4881c251a12c85987bd670c15821dfe47cdbe684
PancrasL <pengzelv@qq.com>
2021-08-31 20:18:22 +0800

    修改为aop代理

---
+++ tcc/src/main/java/io/seata/rm/tcc/rocketmq/RocketMQAop.java
@@ -0,0 +1,24 @@
+package io.seata.rm.tcc.rocketmq;
+
+import org.apache.rocketmq.client.producer.SendResult;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class RocketMQAop {
+    @Around("execution(* org.apache.rocketmq.client.producer.DefaultMQProducer.send(..))")
+    public SendResult doSomething(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("@Around：执行目标方法之前...");
+        //访问目标方法的参数：
+        Object[] args = point.getArgs();
+        if (args != null && args.length > 0 && args[0].getClass() == String.class) {
+            args[0] = "改变后的参数1";
+        }
+        //用改变后的参数执行目标方法
+        Object returnValue = point.proceed(args);
+        System.out.println("@Around：执行目标方法之后...");
+        System.out.println("@Around：被织入的目标对象为：" + point.getTarget());
+        return (SendResult) returnValue;
+    }
+}
