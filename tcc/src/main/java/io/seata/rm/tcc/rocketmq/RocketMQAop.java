1bc6d878c4352929aecbc173d9c53a352faae764
PancrasL <pengzelv@qq.com>
2021-09-18 10:50:59 +0800

    初步实现TCC集成RocketMQ

--- tcc/src/main/java/io/seata/rm/tcc/rocketmq/RocketMQAop.java
+++ tcc/src/main/java/io/seata/rm/tcc/rocketmq/RocketMQAop.java
@@ -4,22 +4,34 @@ import org.apache.rocketmq.client.producer.SendResult;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.seata.core.context.RootContext;
 
 @Aspect
 public class RocketMQAop {
-    TCCRocketMQ tccRocketMQ = new TCCRocketMQImpl();
+    private static final Logger LOGGER = LoggerFactory.getLogger(RocketMQAop.class);
+
+    @Autowired
+    TCCRocketMQ tccRocketMQ;
 
     @Around("execution(* org.apache.rocketmq.client.producer.DefaultMQProducer.send(org.apache.rocketmq.common.message.Message))")
-    // TODO：将RocketMQ纳入到全局事务中
     public SendResult send(ProceedingJoinPoint point) throws Throwable {
         if (RootContext.inGlobalTransaction()) {
-            System.out.printf("In Global Transaction");
-            Object returnValue = point.proceed(point.getArgs());
+            LOGGER.info("DefaultMQProducer send is in Global Transaction, send will be proxy");
+            Map<String, Object> map = new HashMap<>();
+            map.put("defaultMQProducer", point.getTarget());
+            map.put("message", point.getArgs()[0]);
+            tccRocketMQ.prepare(null, map);
+            Object returnValue = map.get("sendResult");
             return (SendResult) returnValue;
         } else {
-            System.out.printf("Not In Global Transaction");
+            LOGGER.info("DefaultMQProducer send not in Global Transaction, use origin send");
             return (SendResult) point.proceed(point.getArgs());
         }
     }
--- tcc/src/main/java/io/seata/rm/tcc/rocketmq/TCCRocketMQImpl.java
+++ tcc/src/main/java/io/seata/rm/tcc/rocketmq/TCCRocketMQImpl.java
@@ -6,33 +6,51 @@ import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import io.seata.rm.tcc.api.BusinessActionContext;
 
 public class TCCRocketMQImpl implements TCCRocketMQ {
     private static final Logger LOGGER = LoggerFactory.getLogger(TCCRocketMQImpl.class);
 
-    private DefaultMQProducer defaultMQProducer;
-    private Message msg = null;
-    private SendResult sendResult = null;
+    private final ConcurrentHashMap<String, Map<String, Object>> map = new ConcurrentHashMap<>();
 
     @Override
-    public boolean prepare(BusinessActionContext actionContext, DefaultMQProducer defaultMQProducer, Message msg) {
-        this.defaultMQProducer = defaultMQProducer;
-        this.msg = msg;
-        this.sendResult = MQUtils.halfSend(defaultMQProducer, msg);
-
+    public boolean prepare(BusinessActionContext context, Map<String, Object> params) {
+        String key = context.getXid() + context.getBranchId();
+        DefaultMQProducer producer = (DefaultMQProducer) params.get("defaultMQProducer");
+        Message message = (Message) params.get("message");
+        SendResult sendResult = MQUtils.halfSend(producer, message);
+        params.put("sendResult", sendResult);
+        LOGGER.info("rocketmq send prepare");
+        map.put(key, params);
         return true;
     }
 
     @Override
-    public boolean commit(BusinessActionContext actionContext) {
-        MQUtils.confirm(defaultMQProducer, msg, sendResult);
+    public boolean commit(BusinessActionContext context) {
+        String key = context.getXid() + context.getBranchId();
+        Map<String, Object> params = map.get(key);
+        DefaultMQProducer producer = (DefaultMQProducer) params.get("defaultMQProducer");
+        Message message = (Message) params.get("message");
+        SendResult sendResult = (SendResult) params.get("sendResult");
+        MQUtils.confirm(producer, message, sendResult);
+        map.remove(key);
+        LOGGER.info("RocketMQ send commit, xid = [{}], branchId = [{}]", context.getXid(), context.getBranchId());
         return true;
     }
 
     @Override
-    public boolean rollback(BusinessActionContext actionContext) {
-        MQUtils.cancel(defaultMQProducer, msg, sendResult);
+    public boolean rollback(BusinessActionContext context) {
+        String key = context.getXid() + context.getBranchId();
+        Map<String, Object> params = map.get(key);
+        DefaultMQProducer producer = (DefaultMQProducer) params.get("defaultMQProducer");
+        Message message = (Message) params.get("message");
+        SendResult sendResult = (SendResult) params.get("sendResult");
+        MQUtils.cancel(producer, message, sendResult);
+        map.remove(key);
+        LOGGER.info("RocketMQ send commit, xid = [{}], branchId = [{}]", context.getXid(), context.getBranchId());
         return true;
     }
 }
